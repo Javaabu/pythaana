2713b390ddd589204f4e6bef795fabb07dab1920
Arushad Ahmed <dash-8x@hotmail.com>
2023-09-18 15:24:49 +0500

    - Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# PyThaana
+
+Thaana helper functions for Python
+
+## Installation
+
+You can install the package using pip
+```bash
+pip install pythaana
+```
+Or install the latest dev version from github
+```bash
+pip install git+https://github.com/javaabu/pythaana.git@master
+```
+## Credits
+- [Arushad Ahmed (@dash8x)](https://arushad.org)  
+
+## About Javaabu
+[Javaabu](https://javaabu.com) is a software development firm based in the Maldives
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(name='pythaana',
+                 version='0.1.0',
+                 description='Python Thaana Helpers',
+                 long_description=open('README.md').read().strip(),
+                 author='Arushad Ahmed',
+                 author_email='arushad@javaabu.com',
+                 url='https://github.com/Javaabu/pythaana',
+                 py_modules=['pythaana'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='thaana text dhivehi transliterator',
+                 classifiers=['Thaana', 'Dhivehi'])
